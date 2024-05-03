bea30fb23ca734a95703c56b05ef988e73f0566b
Naveenkumar <naveen1321311@gmail.com>
2024-05-03 12:50:23 +0530

    navbar created

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import Signup from './components/Authentication/Signup';
 import Signin from './components/Authentication/Signin';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
     <Routes>
+      
       <Route path='/signin' element={<Signin/>}/>
       <Route path='/signup' element={<Signup/>}/>
     </Routes>
