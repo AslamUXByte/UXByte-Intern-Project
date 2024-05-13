490e8d6393321ff51ba129f2d6d0815c2b0b2db5
Naveenkumar <naveen1321311@gmail.com>
2024-05-13 09:55:02 +0530

    hotdeals completed

--- src/components/Landingpage/Featuredprdts.js
+++ src/components/Landingpage/Featuredprdts.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Typography, Grid, Paper, Avatar,Box } from '@mui/material';
+import iphoneImage from './iphone1.png';
+
+
+
+
 
 const Featuredprdts = () => {
   return (
-    <div>Featuredprdts</div>
+    <Paper elevation={3} sx={{ p: 2, bgcolor: 'background.paper' }}>
+      <Grid container spacing={2} sx={{  textAlign: 'center', height: '50vh', width: '1200px', margin: '60px', marginLeft: '130px' }}>
+      <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', textAlign: 'left' }}>
+        <Typography sx={{ color: '#212B36', fontSize: '30px', fontWeight: '600',marginRight:'1000px' }}>
+          Featured Products
+        </Typography>
+      </Grid>
+      </Grid>
+      <Grid container spacing={10} sx={{ overflowX: 'visible' }}>
+        <Grid item xs={6} sm={6} md={4}>
+          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: 4, height: '40vh',width: '150px' }}>
+            <Box sx={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
+            <img style={{ width: '50%' }} src={iphoneImage} alt="Apple iPhone" />
+            <Typography variant="body1" fontWeight="none">Apple iPhone</Typography>
+            <Typography variant="body1" color="orange" gutterBottom>$83.74</Typography>
+            <Typography variant="body2" color="zinc.500">497 Sold</Typography>
+            </Box>
+          </Paper>
+        </Grid>
+
+
+
+
+
+
+     </Grid>  
+  </Paper>
   )
 }
 
--- src/components/Landingpage/hotdeals.js
+++ src/components/Landingpage/hotdeals.js
@@ -4,20 +4,24 @@ import iphoneImage from './iphone1.png';
 import headphoneImage from './headphone2.png'; // Import the image
 import iwatch from './iwatch.png';
 import airpodes from './airpods.png';
+import nikeairmax from './monitor1.png';
+import Applemac from './mac1.png';
 
 const HotDeal = () => {
   return (
-    <Paper elevation={3} sx={{ p: 3, bgcolor: 'background.paper' }}>
-      <Grid container alignItems="center" justifyContent="space-between" sx={{ mb: 4 }}>
-        <Grid item container alignItems="center" spacing={2}>
-         <Typography variant="h5" fontWeight="bold">Hot Deal Today</Typography>
+    <Paper elevation={3} sx={{ p: 2, bgcolor: 'background.paper' }}>
+      <Grid container spacing={2} sx={{  textAlign: 'center', height: '5vh', width: '1200px', margin: '60px', marginLeft: '130px' }}>
+      <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', textAlign: 'left' }}>
+        <Typography sx={{ color: '#212B36', fontSize: '30px', fontWeight: '600',marginRight:'1000px' }}>
+          Hotdeals
+        </Typography>
           <Avatar sx={{ bgcolor: 'white' }}>🔥</Avatar>
           <Typography variant="h5" fontWeight="bold" color="error">01:29:33</Typography>
         </Grid>
        </Grid>
-      <Grid container spacing={16} sx={{ overflowX: 'visible' }}>
-        <Grid item xs={12} sm={6} md={2}>
-          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: 4, height: '80%',width: '200px' }}>
+      <Grid container spacing={10} sx={{ overflowX: 'visible' }}>
+        <Grid item xs={6} sm={4} md={2}>
+          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: 4, height: '80%',width: '150px' }}>
             <Box sx={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
             <img style={{ width: '50%' }} src={iphoneImage} alt="Apple iPhone" />
             <Typography variant="body1" fontWeight="none">Apple iPhone</Typography>
@@ -26,8 +30,8 @@ const HotDeal = () => {
             </Box>
           </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={2}>
-          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: '100%', height: '80%',width: '200px' }}>
+        <Grid item xs={6} sm={4} md={2}>
+          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: 4, height: '80%',width: '150px' }}>
           <Box sx={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
             <img style={{ width: '50%' }} src={headphoneImage} alt="Samsung Galaxy" />
             <Typography variant="body1" fontWeight="none">Samsung Galaxy</Typography>
@@ -36,8 +40,8 @@ const HotDeal = () => {
             </Box>
           </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={2}>
-          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: '100%', height: '80%',width: '200px' }}>
+        <Grid item xs={6} sm={4} md={2}>
+          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: '100%', height: '80%',width: '150px' }}>
           <Box sx={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
             <img style={{ width: '50%' }} src={iwatch} alt="Apple Iwatch" />
             <Typography variant="body1" fontWeight="none">Apple Iwatch</Typography>
@@ -46,13 +50,33 @@ const HotDeal = () => {
             </Box>
           </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: '100%', height: '80%',width: '200px' }}>
+        <Grid item xs={6} sm={4} md={2}>
+          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: '100%', height: '80%',width: '150px' }}>
           <Box sx={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
             <img style={{ width: '50%' }} src={airpodes} alt="Apple Iwatch" />
             <Typography variant="body1" fontWeight="none">Adidas Ultraboost</Typography>
             <Typography variant="body1" color="orange" gutterBottom>$78.99</Typography>
             <Typography variant="body2" color="zinc.500">234 Sold</Typography>
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={4} md={2}>
+          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: '100%', height: '80%',width: '150px' }}>
+          <Box sx={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
+            <img style={{ width: '50%' }} src={nikeairmax} alt="Nike Air Max" />
+            <Typography variant="body1" fontWeight="none">Nike Air Max</Typography>
+            <Typography variant="body1" color="orange" gutterBottom>$568.99</Typography>
+            <Typography variant="body2" color="zinc.500">256 Sold</Typography>
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={4} md={2}>
+          <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: '100%', height: '80%',width: '150px' }}>
+          <Box sx={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
+            <img style={{ width: '50%' }} src={Applemac} alt="Apple mac" />
+            <Typography variant="body1" fontWeight="none">Apple Mac</Typography>
+            <Typography variant="body1" color="orange" gutterBottom>$89.99</Typography>
+            <Typography variant="body2" color="zinc.500">166 Sold</Typography>
             </Box>
           </Paper>
         </Grid>
