aec164a023672fd0c1e2da63abdc1ab15ef69239
Naveenkumar <naveen1321311@gmail.com>
2024-05-16 19:37:03 +0530

    updated featured products grid

--- src/components/Landingpage/Featuredprdts.js
+++ src/components/Landingpage/Featuredprdts.js
@@ -9,17 +9,17 @@ import controller from './controller.png';
 
 const Featuredprdts = () => {
   return (
-    <Paper elevation={3} sx={{ p: 2, bgcolor: 'background.paper', margin: '50px', marginLeft: '50px', marginRight: '50px', height: '700px',boxShadow:4 }}>
+    <Paper elevation={3} sx={{ p: 2, bgcolor: 'background.paper', margin:'50px' ,width:'850px', height: '700px',boxShadow:0 }}>
       <Grid container spacing={2} direction ="row">
         <Grid item xs={2}>
           <Typography gutterBottom variant='h6' sx={{ color: '#212B36', fontSize: '30px', fontWeight: '600' }}>
-            Featured Products
+            FeaturedProducts
           </Typography>
         </Grid>
       </Grid>
       <Grid container item xs={20} spacing={2} alignItems="row">
-        <Grid item xs={3.5}>
-          <Paper sx={{ p: 0, bgcolor: '#e5b75c', borderRadius: '12px', boxShadow: 4, height: '600px', width: '370px',":hover":{border:'2px solid black',borderColor: 'black'} }}>
+        <Grid item xs={6}>
+          <Paper sx={{ p: 2, bgcolor: '#e5b75c', borderRadius: '12px', boxShadow: 4, height: '600px', width: '370px',":hover":{border:'2px solid black',borderColor: 'black'} }}>
             <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
               <img style={{ width: '370px', height: '400px' }} src={iphoneImage} alt="Apple iPhone" />
               <Typography variant="body1" fontWeight={80}>Apple iPhone</Typography>
@@ -28,8 +28,8 @@ const Featuredprdts = () => {
             </Box>
           </Paper>
         </Grid>
-        <Grid item xs={3.5}>
-          <Paper sx={{ p: 0, bgcolor: '#8b4fb6', borderRadius: '12px', boxShadow: 4, height: '600px', width: '370px',":hover":{border:'2px solid black',borderColor: 'black'} }}>
+        <Grid item xs={6}>
+          <Paper sx={{ p: 2, bgcolor: '#8b4fb6', borderRadius: '12px', boxShadow: 4, height: '600px', width: '370px',":hover":{border:'2px solid black',borderColor: 'black'} }}>
             <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
               <img style={{ width: '370px', height: '400px' }} src={headphoneImage} alt="Samsung headphones" />
               <Typography variant="body1" fontWeight={80}>Samsung headphones</Typography>
@@ -38,6 +38,9 @@ const Featuredprdts = () => {
             </Box>
           </Paper>
         </Grid>
+      </Grid>
+      <Paper elevation={3} sx={{ p: 2, bgcolor: 'background.paper', margin:'50px' ,width:'850px', height: '700px',boxShadow:0 }}>
+      <Grid container spacing={2} direction ="row">    
         <Grid item xs={3.5} sm={4} md={2}>
           <Paper sx={{ p: 2, bgcolor: 'zinc.100', borderRadius: '12px', boxShadow: 4, height: '220px',width: '200px',":hover":{border:'2px solid black',borderColor: 'black'} }}>
           <Box sx={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
@@ -79,6 +82,7 @@ const Featuredprdts = () => {
           </Paper>
         </Grid>
       </Grid>
+    </Paper>
     </Paper>
   );
 };
